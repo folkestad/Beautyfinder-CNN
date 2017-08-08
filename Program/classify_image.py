9d691a7548f1523ba47d7295e1fb7bdd27fe7db2
Eirik Folkestad <eirik.ek.folkestad@gmail.com>
2017-08-09 11:58:31 +1200

    Add code for finding facial landmarks

--- Program/classify_image.py
+++ Program/classify_image.py
@@ -1,6 +1,6 @@
 import numpy
 import os
-import cv2 as opencv
+import cv2
 import tensorflow as tf
 from CNN import *
 from HAAR import *
@@ -8,8 +8,8 @@ from HAAR import *
 def classify_image_to_beauty_scale(file_name="test.jpg", dim1=32, dim2=32):
 
     # img = get_image(file_name=file_name)
-    img = haar_cascade(dir_name="Test", file_name=file_name)
-    resized_image = resize_image(img, dim1, dim2)
+    haar_img = haar_cascade(dir_name="Test", file_name=file_name)
+    resized_image = resize_image(haar_img, dim1, dim2)
 
     x = tf.placeholder(tf.float32, [None, resized_image.shape[0], resized_image.shape[1], resized_image.shape[2]])
     y_conv, keep_prob = cnn_model(x, resized_image.shape[0], resized_image.shape[1], resized_image.shape[2], 11)
@@ -23,6 +23,14 @@ def classify_image_to_beauty_scale(file_name="test.jpg", dim1=32, dim2=32):
             keep_prob: 1.0
         })
         print("The person in the image is a {} on the beauty scale.".format(result))
+    
+    current_dir = os.path.dirname(__file__)
+    file_path = '../Data/Test/{}'.format(file_name)
+    file_rel_path = os.path.join(current_dir, file_path)
+    img = cv2.imread(file_rel_path)
+    print(type(img), img.shape)
+    show_image(img)
+
 
 
 def get_model_path(file_name='model.ckpt'):
@@ -35,20 +43,20 @@ def get_model_path(file_name='model.ckpt'):
     return file_rel_path
 
 def show_image(img, name='Picture'):
-    opencv.imshow(name, img)
-    opencv.waitKey(0)
-    opencv.destroyAllWindows()
+    cv2.imshow(name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 def get_image(file_name):
     current_dir = os.path.dirname(__file__)
     file_path = '../Data/Test/{}'.format(file_name)
     file_rel_path = os.path.join(current_dir, file_path)
-    img = opencv.imread(file_rel_path)
+    img = cv2.imread(file_rel_path)
     return img
 
 def resize_image(img, dim1, dim2):
-    resized_img = opencv.resize(img, (dim2, dim1)) 
+    resized_img = cv2.resize(img, (dim2, dim1)) 
     return resized_img
 
 if __name__ == '__main__':
-    classify_image_to_beauty_scale(file_name="test.jpg", dim1=32, dim2=32)
+    classify_image_to_beauty_scale(file_name="b1.jpg", dim1=32, dim2=32)
